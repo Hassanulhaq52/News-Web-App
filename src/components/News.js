a9152794403eacc7ef6c1855a5a70932285ef0c6
Hassan Ul Haq <81625175+Hassanulhaq52@users.noreply.github.com>
2022-06-15 01:18:14 +0500

    Adding Categories & propTypes to NewsMonkey React App

--- src/components/News.js
+++ src/components/News.js
@@ -1,8 +1,23 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
+import PropTypes from 'prop-types'
 
 export class News extends Component {
+
+static defaultProps = {
+  country: 'in',
+  pageSize: 8, 
+  category: 'general',
+}
+
+static propTypes = {
+  country: PropTypes.string,
+  pageSize: PropTypes.number, 
+  category: PropTypes.string,
+}
+
+
   constructor(){
     super();
     this.state = {
@@ -13,7 +28,7 @@ export class News extends Component {
 }
 
 async componentDidMount(){ 
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=fb3dda4660964549935676992a2efb63&page=1&pageSize=${this.props.pageSize}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=fb3dda4660964549935676992a2efb63&page=1&pageSize=${this.props.pageSize}`;
     
   this.setState({loading: true});
   let data = await fetch(url);
@@ -29,7 +44,7 @@ async componentDidMount(){
    handlePrevClick = async ()=>{
     console.log("Previous");
 
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=fb3dda4660964549935676992a2efb63&page=${
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=fb3dda4660964549935676992a2efb63&page=${
       this.state.page - 1
     }&pageSize=${this.props.pageSize}`;
   
@@ -52,7 +67,7 @@ handleNextClick = async ()=>{
     if (!(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize))){
     
 
-      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=fb3dda4660964549935676992a2efb63&page=${
+      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=fb3dda4660964549935676992a2efb63&page=${
         this.state.page + 1
       }&pageSize=${this.props.pageSize}`;
 
@@ -72,7 +87,7 @@ handleNextClick = async ()=>{
   render() {
     return (
       <div className="container my-3">
-        <h1 className="text-center">NewsMonkey - Top Headlines</h1>
+        <h1 className="text-center" style={{margin:'35px 0px'}}>NewsMonkey - Top Headlines</h1>
 
 
         {this.state.loading && <Spinner/>}
